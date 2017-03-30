23e371b728b9ca46e4eeb733a510a81272683775
Robert Willems of Brilman <robert.willems.of.brilman@gmail.com>
2017-03-30 16:36:57 +0200

    Remove unused import

--- src/test/java/nl/futureedge/sonar/plugin/issueresolver/PluginIT.java
+++ src/test/java/nl/futureedge/sonar/plugin/issueresolver/PluginIT.java
@@ -30,7 +30,6 @@ import org.apache.http.util.EntityUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
