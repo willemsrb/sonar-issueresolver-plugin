19ec33655ecf35ad7d9699fb8c604790135a7dae
Robert Willems of Brilman <robert.willems.of.brilman@gmail.com>
2017-04-06 13:54:38 +0200

    Fix breaking bug when using a context path

--- src/main/resources/static/entrypoint.js
+++ src/main/resources/static/entrypoint.js
@@ -2,9 +2,11 @@ window.registerExtension('issueresolver/entrypoint', function (options) {
 	options.el.id='issueresolver-page';
 	options.el.className='page page-limited';
 	
+	var location = options.router.createLocation('/static/issueresolver');
+
 	var loader = function() {
 		requirejs.config({
-		    baseUrl: '/static/issueresolver'
+		    baseUrl: location.basename + location.pathname
 		});
 		requirejs(['main'], function(main) {
 			main.main(options);
@@ -14,7 +16,7 @@ window.registerExtension('issueresolver/entrypoint', function (options) {
     // Adding the script tag to the head as suggested before
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = '/static/issueresolver/require.js';
+    script.src = location.basename + location.pathname + '/require.js';
 
     // Then bind the event to the callback function.
     // There are several events for cross browser compatibility.
